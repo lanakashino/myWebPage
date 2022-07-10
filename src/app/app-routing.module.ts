18523391c4b38ab41024f5bd38c897e40c5f72bf
Lana <lanakash@students.cs.ubc.ca>
2022-07-09 17:06:32 -0700

    add base structure of my app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProjectsComponent} from "./projects/projects.component";
+import {ExperiencesComponent} from "./experiences/experiences.component";
+import {EducationComponent} from "./education/education.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'experiences', component: ExperiencesComponent},
+  { path: 'education', component: EducationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EducationComponent,
+    ProjectsComponent,
+    ExperiencesComponent
   ],
   imports: [
     BrowserModule,
